6e7191e1ba3fee0a888b81f49526d69714680bb7
Val Patterson <val@nutznboltz.website>
2021-06-26 21:57:50 -0400

    add playback speed functionality

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
+const speed = document.querySelector('.player-speed');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
@@ -104,8 +105,9 @@ function toggleMute() {
 }
 
 // Change Playback Speed -------------------- //
-
-
+function changeSpeed() {
+    video.playbackRate = speed.value;
+}
 
 // Fullscreen ------------------------------- //
 
@@ -118,3 +120,4 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
